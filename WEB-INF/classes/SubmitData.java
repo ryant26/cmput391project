f7cc2e3a3b376c006b76a63d8414657fbd417640
ryant26 <ryant26@gmail.com>
2015-04-01 12:25:59 -0600

    Error responses working

--- WEB-INF/classes/SubmitData.java
+++ WEB-INF/classes/SubmitData.java
@@ -17,10 +17,113 @@ import db.Database;
 * area.
 */
 public class SubmitData extends HttpServlet{
+
+	HttpServletResponse response;
+
 	 public void doPost(HttpServletRequest request, HttpServletResponse response) 
 	 throws ServletException, IOException{
-	 	String data = request.getParameter("value");
-	 	System.out.println("Received Data: \n" + data);
-	 	response.sendError(HttpServletResponse.SC_OK);
+	 	this.response = response;
+	 	response.setContentType("text/html");
+
+	 	String value = request.getParameter("value");
+	 	String pk = request.getParameter("pk");
+	 	String name = request.getParameter("name");
+	 	System.out.println("DEBUG received values: " + pk + " " + name + " " + value);
+	 	if (name.equals("username")){
+	 		handleUsername(pk, value);
+	 	} else if (name.equals("password")) {
+	 		handlePassword(pk, value);
+	 	} else if (name.equals("date")){
+	 		handleDateRegistered(pk, value);
+	 	} else if (name.equals("class")) {
+	 		handleClass(pk, value);
+	 	}
 	}
+
+	private void handleUsername(String pk, String value){
+		String query = buildUsersQuery("user_name", value, pk);
+		if (noSpaces(value)){
+			runQuery(query);
+		} else {
+			sendFailure("No spaces allowed!");
+		}
+	}
+
+	private void handlePassword(String pk, String value){
+		String query = buildUsersQuery("password", value, pk);
+		if (noSpaces(value)){
+			runQuery(query);
+		} else {
+			sendFailure("No spaces allowed!");
+		}
+	}
+
+	private void handleDateRegistered(String pk, String value){
+	}
+
+	private void handleClass(String pk, String value){
+		String query = buildUsersQuery("class", value, pk);
+		if (length(value, 1)){
+			runQuery(query);
+		} else {
+			sendFailure("Must be 1 character");
+		}
+	}
+
+	private boolean noSpaces(String input){
+		if(input.split(" ").length > 1){
+			return false;
+		}
+		return true;
+	}
+
+	private boolean length(String input, int len){
+		if (input.length() <= len){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void runQuery(String query){
+		Database db = new Database();
+		System.out.println("DEBUG, query string: " + query);
+		try{
+			Connection conn = db.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet results = stmt.executeQuery(query);
+			sendOK();
+		} catch (Exception e){
+			System.out.println("Error updating data: " + e.getMessage());
+			sendFailure("That input is not correct");
+		} finally {
+			db.close();
+		}
+	}
+
+	private String buildUsersQuery(String field, String value, String pk){
+		return buildQuery("users", field, value, "person_id", pk);
+	}
+
+	private String buildQuery(String table, String field, String value, String pk, String pk_value){
+		return "UPDATE "+ table +" SET "+ field +"= '" + value + "' where "+ pk +" = " + pk_value;
+	}
+
+	private void sendOK(){
+		try{
+	 		response.sendError(HttpServletResponse.SC_OK);
+	 	} catch (IOException e){
+	 		System.out.println("Error sending repsonse: " + e.getMessage());
+	 	}
+	}
+
+	private void sendFailure(String msg){
+		try{
+	 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+ 			response.getWriter().print(msg);
+	 	} catch (IOException e){
+	 		System.out.println("Error sending repsonse: " + e.getMessage());
+	 	}
+	}
+
 }  
